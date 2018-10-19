d51078ad03e9256acd0ed11b17802e72eb6a41a8
hnah <hnahoulder@gmail.com>
2018-10-20 00:00:54 +0200

    section 3, partie 8

--- app.js
+++ app.js
@@ -2,8 +2,35 @@ const express = require('express');
 const app = express();
 
 const PORT = 3000;
+
+app.use('/public', express.static('public')); //middleware pour utiliser les fichiers statiques (.css)
+
+app.set('views', './views'); //déclaration du répertoire views
+app.set('view engine', 'ejs')//view engine c'est EJS npm install ejs
+
+
+app.get('/movies', (req, res) => {
+    // res.send('Bientôt des films ici même');
+    res.render('movies');
+});
+
+/*app.get('/movie-details', (req, res) => {
+   res.render('movie-details');
+});*/
+
+app.get('/movies/add', (req, res) => {
+    res.send(`Formule d'ajout ici`);
+});
+
+app.get('/movies/:id', (req, res) => {
+    // const id = req.params.id;
+    // res.send(`Film numéro ${id}`);
+    res.render('movie-details');
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello <b>world!</b>');
+    // res.send('Hello <b>world!</b>');
+    res.render('index');
 });
 
 app.listen(3000, () => {
