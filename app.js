50b1d92f891581da4adf8811d9793b269818e749
houlder.hambalaye <hnahoulder@gmail.com>
2018-10-19 16:30:47 +0200

    amend

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
 
+const PORT = 3000;
 app.get('/', (req, res) => {
     res.send('Hello <b>world!</b>');
 });
 
 app.listen(3000, () => {
-    console.log('Listening on port 3000');
+    console.log(`Listening on port ${PORT}`);
 });
